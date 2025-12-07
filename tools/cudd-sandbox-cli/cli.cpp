02005cea9ac3364053a4bcb009cd3bb1dd9cd64d
Jevin Sweval <jevinsweval@gmail.com>
2025-12-07 14:18:39 -0500

    wip

--- tools/cudd-sandbox-cli/cli.cpp
+++ tools/cudd-sandbox-cli/cli.cpp
@@ -1,7 +1,9 @@
 #include <cudd-sandbox/cudd-sandbox.hpp>
 
+#include <flags.h>
 #include <fmt/format.h>
 
 int main(int argc, char **argv) {
+    const flags::args args(argc, argv);
     return 0;
 }
