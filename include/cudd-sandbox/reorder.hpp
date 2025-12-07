8144b67e17be1fd17dbccce8209483ba0be0c0be
Jevin Sweval <jevinsweval@gmail.com>
2025-12-07 15:50:39 -0500

    doh

--- include/cudd-sandbox/reorder.hpp
+++ include/cudd-sandbox/reorder.hpp
@@ -2,5 +2,4 @@
 
 #include <string_view>
 
-[[gnu::visibility("default")]]
 bool reorder_dddmp_file(const std::string_view &in, const std::string_view &out_path);
--- lib/reorder.cpp
+++ lib/reorder.cpp
@@ -1,6 +1,5 @@
 #include <cudd-sandbox/reorder.hpp>
 
-[[gnu::visibility("default")]]
 bool reorder_dddmp_file(const std::string_view &in_path, const std::string_view &out_path) {
     return true;
 }
