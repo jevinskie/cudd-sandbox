eca0bdff110c09747b83dfebb1cfb2d6b6e32ba7
Jevin Sweval <jevinsweval@gmail.com>
2025-12-10 14:10:52 -0500

    pla_file ctre skeleton

--- lib/pla_file.cpp
+++ lib/pla_file.cpp
@@ -1,5 +1,18 @@
 #include <cudd-sandbox/pla_file.hpp>
 
+#include <optional>
+
+#include <ctre.hpp>
+
+std::optional<std::string_view> extract_number(std::string_view s) noexcept {
+    using namespace ctre::literals;
+    if (auto m = ctre::match<"^[a-z]++([0-9]++)$">(s)) {
+        return m.get<1>().to_view();
+    } else {
+        return std::nullopt;
+    }
+}
+
 PLA read_pla_file(std::string_view pla_path) {
     PLA pla;
     return pla;
