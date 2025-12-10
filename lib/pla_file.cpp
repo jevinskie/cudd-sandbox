dc1d42689083bc5759b6cd14220224d05fb9700a
Jevin Sweval <jevinsweval@gmail.com>
2025-12-10 15:41:54 -0500

    cleanup

--- lib/pla_file.cpp
+++ lib/pla_file.cpp
@@ -1,14 +1,13 @@
 #include <cudd-sandbox/pla_file.hpp>
 
 #include <optional>
-#include <utility>
 
 #include <ctre.hpp>
 
-std::optional<std::pair<std::string_view, std::string_view>> extr_implicant(std::string_view s) noexcept {
+std::optional<Implicant> extr_implicant(std::string_view s) noexcept {
     using namespace ctre::literals;
     if (auto m = ctre::match<R"re(^\s*([01\-]+)\s+([01~]+)\s*$)re">(s)) {
-        return std::make_pair(m.get<1>().to_view(), m.get<2>().to_view());
+        return Implicant(m.get<1>().to_view(), m.get<2>().to_view());
     } else {
         return std::nullopt;
     }
