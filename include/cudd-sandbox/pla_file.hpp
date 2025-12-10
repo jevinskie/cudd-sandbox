5d5208469c00c98b1810941ec22b7f02e8b27f64
Jevin Sweval <jevinsweval@gmail.com>
2025-12-10 14:09:03 -0500

    read_pla_file skeleton

--- include/cudd-sandbox/pla_file.hpp
+++ include/cudd-sandbox/pla_file.hpp
@@ -1,3 +1,7 @@
 #pragma once
 
 #include <cudd-sandbox/sop.hpp>
+
+#include <string_view>
+
+PLA read_pla_file(std::string_view pla_path);
---
+++ lib/pla_file.cpp
@@ -0,0 +1,6 @@
+#include <cudd-sandbox/pla_file.hpp>
+
+PLA read_pla_file(std::string_view pla_path) {
+    PLA pla;
+    return pla;
+}
