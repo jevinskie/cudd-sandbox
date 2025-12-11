62ac563bf5566080d5fc8873b732c9670bbe7dc8
Jevin Sweval <jevinsweval@gmail.com>
2025-12-11 15:04:52 -0500

    PLA implication parsing works

--- lib/pla_file.cpp
+++ lib/pla_file.cpp
@@ -2,13 +2,16 @@
 
 #include <cudd-sandbox/utils.hpp>
 
+#include <cinttypes>
 #include <exception>
 #include <optional>
+#include <ranges>
 
 #include <ctre.hpp>
+#include <string>
+#include <string_view>
 
 std::optional<Implicant> extr_implicant(std::string_view s) noexcept {
-    using namespace ctre::literals;
     if (auto m = ctre::match<R"re(^\s*([01\-]+)\s+([01~]+)\s*$)re">(s)) {
         return Implicant(m.get<1>().to_view(), m.get<2>().to_view());
     } else {
@@ -22,6 +25,16 @@ SOP read_pla_file(const std::string &pla_path) {
     if (!plas.size()) {
         fprintf(stderr, "pla file read error\n");
         std::terminate();
+    }
+    int i = 0;
+    for (const auto &line : plas | std::ranges::views::split('\n')) {
+        ++i;
+        printf("pla[%5d]_i: %.*s\n", i, static_cast<int>(line.size()), line.data());
+        if (const auto imp = extr_implicant(std::string_view(line))) {
+            // printf("pla[%5d]_o: ibm: 0x%08" PRIx64 " ibp: 0x%08" PRIx64 " obm: 0x%08" PRIx64 " obp: 0x%08" PRIx64
+            //         "\n", i, imp->in_bmask(), imp->in_bpat(), imp->out_bmask(), imp->out_bpat());
+            printf("pla[%5d]_o: b[m,p]: 0x%08" PRIx64 ", 0x%08" PRIx64 "\n", i, imp->in_bmask(), imp->in_bpat());
+        }
     }
     return pla;
 }
--- tools/cudd-sandbox-cli/cli.cpp
+++ tools/cudd-sandbox-cli/cli.cpp
@@ -1,3 +1,4 @@
+#include "cudd-sandbox/pla_file.hpp"
 #include <cudd-sandbox/cudd-sandbox.hpp>
 
 #include <cudd.h>
@@ -9,19 +10,27 @@ int main(int argc, char **argv) {
     const auto in_path     = args.get<std::string>("in");
     const auto out_path    = args.get<std::string>("out");
     const auto reorder_cmd = args.get<bool>("reorder", false);
+    const auto pla_cmd     = args.get<bool>("pla", false);
     if (reorder_cmd) {
         if (!in_path) {
-            fmt::print(stderr, "need --in path\n");
+            fmt::print(stderr, "reorder: need --in path\n");
             return 1;
         }
         if (!out_path) {
-            fmt::print(stderr, "need --out path\n");
+            fmt::print(stderr, "reorder: need --out path\n");
             return 1;
         }
         fmt::print("reordering {:s} into {:s}\n", *in_path, *out_path);
         DdManager *mgr        = Cudd_Init(0, 0, CUDD_UNIQUE_SLOTS, CUDD_CACHE_SLOTS, 0);
         const int reorder_res = reorder_dddmp_file(mgr, *in_path, *out_path);
         fmt::print("result: {}\n", reorder_res);
+    } else if (pla_cmd) {
+        if (!in_path) {
+            fmt::print(stderr, "pla: need --in path\n");
+            return 1;
+        }
+        fmt::print("pla reading {:s} into {:s}\n", *in_path, *out_path);
+        read_pla_file(*in_path);
     }
     return 0;
 }
